15ed2dc5d99cc71447bead41d3ec62c05a06474f
Ray Agni <jagni001@ucr.edu>
2021-11-07 01:29:29 -0800

    feat: added formatted messages to homepage

--- app/containers/StringStoreHomePage/index.js
+++ app/containers/StringStoreHomePage/index.js
@@ -4,8 +4,6 @@
  * render the input component that will be used to submit a string to the
  * database.
  *
- * Renders:
- *  App -> StringStoreHomePage ->
  */
 
 import React, { memo } from 'react';
@@ -19,7 +17,7 @@ import { createStructuredSelector } from 'reselect';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
-// TODO: create messages for the fields.
+import { FormattedMessage } from 'react-intl';
 import saga from './saga';
 import reducer from './reducer';
 import { changeString } from './action';
@@ -27,52 +25,50 @@ import { makeSelectString } from './selectors';
 
 import { postString } from '../App/actions';
 
-// Styled Components
 import Section from './Section';
 import CenteredSection from './CenteredSection';
 import Input from './Input';
 import H2 from '../../components/H2';
 import Form from './Form';
 import { makeSelectLoading } from '../App/selectors';
+import messages from './messages';
 
 // key used for reducer
 const key = 'stringstorehome';
 
 StringStoreHomePage.propTypes = {
-  loading: PropTypes.bool,
   string: PropTypes.string,
   onChangeString: PropTypes.func,
   onSubmitForm: PropTypes.func,
 };
 
-export function StringStoreHomePage({
-  loading,
-  string,
-  onSubmitForm,
-  onChangeString,
-}) {
+export function StringStoreHomePage({ string, onSubmitForm, onChangeString }) {
   useInjectReducer({ key, reducer });
   useInjectSaga({ key, saga });
 
+  // TODO need to add a styled component with conditional rendering!
   return (
     <article>
       <Helmet>
+        <title> String Store Home Page</title>
         <meta name="description" content="String Store Application homepage" />
       </Helmet>
-      {/* TODO: Add components */}
       {/* TODO: add a centered section to add 1. landing statement 2. description 3. input */}
       <div>
         <CenteredSection>
-          {/* TODO Add message H2 */}
-          {/* TODO ADD p  */}
+          <H2>
+            <FormattedMessage {...messages.header} />
+          </H2>
+          <p>
+            <FormattedMessage {...messages.headerMessage} />
+          </p>
         </CenteredSection>
         <Section>
           <H2>{/* Header */}</H2>
         </Section>
         <Form onSubmit={onSubmitForm}>
           <label htmlFor="string">
-            {/* TODO Insert message here */}
-            string:
+            <FormattedMessage {...messages.inputLabel} />
             <Input
               id="string"
               type="text"
@@ -84,10 +80,6 @@ export function StringStoreHomePage({
           {/* TODO add message for submit */}
           <button type="submit"> Submit </button>
           <ToastContainer position="bottom-center" closeOnClick="true" />
-          {/* TODO add message for the I tag  */}
-          <Section>
-            {loading ? <i> Adding string to database...</i> : ' '}
-          </Section>
         </Form>
       </div>
     </article>
---
+++ app/containers/StringStoreHomePage/messages.js
@@ -0,0 +1,19 @@
+import { defineMessages } from 'react-intl';
+
+export const scope = 'stringstore.containers.StringStoreHomePage';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'String Store App',
+  },
+  headerMessage: {
+    id: `${scope}.header-message`,
+    defaultMessage:
+      'An application that allows you to store messages onto the database.',
+  },
+  inputLabel: {
+    id: `${scope}.input-label.message`,
+    defaultMessage: 'String: ',
+  },
+});
