f006622ee9fc86e6ad3925fefbe7e24eb2f35041
Ray Agni <jagni001@ucr.edu>
2021-11-07 18:55:47 -0800

    FEAT: added stringsDisplayTest

--- app/containers/StringsDisplayPage/tests/index.test.js
+++ app/containers/StringsDisplayPage/tests/index.test.js
@@ -1,1 +1,40 @@
-// TODO create a test suite for index
+/**
+ * Test for the StringDisplayPage
+ */
+
+import React from 'react';
+import { render } from 'react-testing-library';
+import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router-dom';
+
+import { StringsDisplayPage, mapDispatchToProps } from '../index';
+// import { loadStrings } from '../../App/actions';
+import configureStore from '../../../configureStore';
+
+describe('<StringDisplayPages />', () => {
+  let store;
+  const onMount = jest.fn();
+
+  beforeAll(() => {
+    store = configureStore({}, browserHistory);
+  });
+
+  it('should render and match the snapshot', () => {
+    const {
+      container: { firstChild },
+    } = render(
+      <Provider store={store}>
+        <IntlProvider locale="en">
+          <StringsDisplayPage
+            onMount={onMount}
+            loading={false}
+            error={false}
+            string={[]}
+          />
+        </IntlProvider>
+      </Provider>,
+    );
+    expect(firstChild).toMatchSnapshot();
+  });
+});
--- server/helper.js
+++ server/helper.js
@@ -4,14 +4,17 @@
 /** updateString
  * Updates the message of a string given the id and array serving as the
  * database. If the id is not found, the function will return -1.
+ *
+ * NOTE: This helper function is for demonstration only in place of ORM/RMDB methods.
  * @param {Array} db: The database with an array of string objects like
  *                     [{id, message}...]
- * @param {Number} id: id of the message to update.
+ * @param {Number} or {String} id: id of the message to update.
  * @param {String} message: message of the string to update.
  * @return {Array} updated db
  *
  * NOTE: Used in ./routes/routes.js
  */
+
 function updateString(db, id, message) {
   for (const entry of db) {
     if (entry.id == id) {
